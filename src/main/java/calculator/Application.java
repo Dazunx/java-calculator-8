6072514bec95a37e6d8f69ed7b01f11a3cfc241c
DajeongLee <dajeong@DajeongLeeui-MacBookPro.local>
2025-10-16 00:16:04 +0900

    feat(separator): define custom separator using string between '//' and '\n'

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        String separator = "";
         String formula = Console.readLine();
+        formula = formula.replace("\\n", "\n");
+
+        int frontCount = (formula.length() - formula.replace("//", "").length())/2;
+        int backCount = formula.length() - formula.replace("\n", "").length();
+
+        // custom separator
+        if (frontCount>=1 && backCount==1){
+            String[] array = formula.split("\n");
+            separator = array[0].substring(2);
+            formula = array[1];
+        } else if (frontCount==1 && backCount>1) { // if custom separator is "\n"
+            separator = "\n";
+            formula = formula.substring(6);
+        }
     }
 }
