e039712d0dcd2275bfff9b9105749d852cd9f716
DajeongLee <dajeong@DajeongLeeui-MacBookPro.local>
2025-10-19 19:03:30 +0900

    test(application): add test for both custom and default separator

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -79,6 +79,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 커스텀과_기본구분자_모두_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
