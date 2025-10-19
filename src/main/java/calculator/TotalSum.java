6cbca55b3b315316369c3ac1f2c6ed0fec8836a3
DajeongLee <dajeong@DajeongLeeui-MacBookPro.local>
2025-10-19 19:13:09 +0900

    fix(TotalSum): throw exception when no valid numbers remain after separation

--- src/main/java/calculator/TotalSum.java
+++ src/main/java/calculator/TotalSum.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class TotalSum {
     public int add(List<String> numbers) throws IllegalArgumentException {
         int total = 0;
+        if (numbers.isEmpty()){
+            throw new IllegalArgumentException();
+        }
         for(String n:numbers){
             if(n.contains("-") || n.contains(("+"))){
                 throw new IllegalArgumentException();
