25e2bc992a03386f30a687534d78fee2a2cd653f
DajeongLee <dajeong@DajeongLeeui-MacBookPro.local>
2025-10-19 18:43:02 +0900

    test(separator): add test for custom separator longer than one character

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -33,6 +33,14 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_한글자이상_사용() {
+        assertSimpleTest(() -> {
+            run("//--\\n1--4--12");
+            assertThat(output()).contains("결과 : 17");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
